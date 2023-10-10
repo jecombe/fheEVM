ed67fe058657e71208e6fd648748b313c9578996
jeremy <jeremcombe@hotmail.fr>
2023-10-11 00:34:20 +0200

    interact

--- fheAMM/interact/index.js
+++ fheAMM/interact/index.js
@@ -25,16 +25,19 @@ const start = async () => {
     console.log("token0 => ", balanceToken0);
     console.log("token1 => ", balanceToken1);
 
-    // await approves(200);
-    // await sleep(1000);
+  //   await approves(200);
+  //   console.log('APPROVES OK');
+  // //  await sleep(1000);
 
-    // await createPool(signer, token0, token1);
-    // await sleep(5000);
+  //   const pool = await createPool(signer, token0, token1);
+  //   console.log(pool, 'POOL OK');
+  //   await sleep(5000);
 
-    // await addLiquidity(signer, 0, 100, 100);
-    // await sleep(5000);
-
-    // await swap(10, token1, 0);
+  //    const liquidity = await addLiquidity(signer, 0, 10, 10);
+  //    console.log(liquidity, 'LIQUIDITY OK');
+  //  await sleep(5000);
+    //  const repSwap  = await swap(signer, 10, token1, 0);
+    //  console.log(repSwap, 'SWAP OK');
   } catch (error) {
     console.log(error);
   }
